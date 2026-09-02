51af305db74edbfbf2e4e56d99e4252f8c14d693
Nagarathinam <shanshia8@gmail.com>
2026-09-03 01:44:03 +0530

    Add Terraform variables

---
+++ terraform/variables.tf
@@ -0,0 +1,23 @@
+variable "aws_region" {
+  description = "AWS region where infrastructure will be created"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "key_name" {
+  description = "Existing AWS EC2 key pair name"
+  type        = string
+  default     = "shivaya"
+}
+
+variable "ami_id" {
+  description = "Amazon Linux AMI ID for ap-south-1"
+  type        = string
+  default     = "ami-0f918f7e67a3323f0"
+}
