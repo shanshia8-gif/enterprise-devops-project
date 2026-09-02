ec1c50ebf51b978afc18c9294972a3bd71fa4b5d
Nagarathinam <shanshia8@gmail.com>
2026-09-02 20:59:13 +0530

    terraform main.tf
    Added resources for VPC, public subnet, internet gateway, route table, security group, and EC2 instance for a DevOps project.

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,6 +13,7 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+# VPC
 resource "aws_vpc" "devops_vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
@@ -22,6 +23,8 @@ resource "aws_vpc" "devops_vpc" {
     Name = "enterprise-devops-vpc"
   }
 }
+
+# Public Subnet
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.devops_vpc.id
   cidr_block              = "10.0.1.0/24"
@@ -32,6 +35,8 @@ resource "aws_subnet" "public_subnet" {
     Name = "enterprise-devops-public-subnet"
   }
 }
+
+# Internet Gateway
 resource "aws_internet_gateway" "devops_igw" {
   vpc_id = aws_vpc.devops_vpc.id
 
@@ -39,6 +44,8 @@ resource "aws_internet_gateway" "devops_igw" {
     Name = "enterprise-devops-internet-gateway"
   }
 }
+
+# Public Route Table
 resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.devops_vpc.id
 
@@ -51,10 +58,14 @@ resource "aws_route_table" "public_route_table" {
     Name = "enterprise-devops-public-route-table"
   }
 }
+
+# Route Table Association
 resource "aws_route_table_association" "public_subnet_association" {
   subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.public_route_table.id
 }
+
+# Security Group
 resource "aws_security_group" "devops_sg" {
   name        = "enterprise-devops-sg"
   description = "Security group for DevOps project"
@@ -87,6 +98,15 @@ resource "aws_security_group" "devops_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Enterprise DevOps Application
+  ingress {
+    description = "Enterprise DevOps Application"
+    from_port   = 8081
+    to_port     = 8081
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   # All outbound traffic
   egress {
     from_port   = 0
@@ -99,6 +119,8 @@ resource "aws_security_group" "devops_sg" {
     Name = "enterprise-devops-security-group"
   }
 }
+
+# EC2 Instance
 resource "aws_instance" "jenkins_server" {
   ami           = "ami-0f918f7e67a3323f0"
   instance_type = "t3.micro"
@@ -113,6 +135,8 @@ resource "aws_instance" "jenkins_server" {
     Name = "Jenkins-Server"
   }
 }
+
+# Outputs
 output "jenkins_public_ip" {
   value = aws_instance.jenkins_server.public_ip
 }
