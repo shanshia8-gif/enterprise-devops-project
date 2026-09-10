14ba515de5267c4e5282fe93df03420c28493549
Rathna <eroderathna2112@gmail.com>
2026-09-11 03:19:25 +0530

    Fix Terraform duplicate configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -77,7 +77,7 @@ resource "aws_security_group" "devops_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["122.171.21.97/32"]
   }
 
   # Jenkins
